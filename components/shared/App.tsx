7c781e45a9fcb9e072995812c3e57ff8dc9b3c81
Hypercodding <mohammadhammad302@gmail.com>
2024-07-11 11:34:58 +0500

    changes

--- components/shared/App.tsx
+++ components/shared/App.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { SignInButton } from "@clerk/clerk-react";
+
 import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
 import LoadingLogo from "./LoadingLogo";
 import "./app.css";
-import { Card } from "../ui/card";
 import { SignIn } from "@clerk/nextjs";
 
 function App({ children }: { children: React.ReactNode }) {
